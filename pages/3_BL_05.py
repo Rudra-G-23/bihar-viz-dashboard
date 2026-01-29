684ad9fd077d60f281dc9587d8999dcccc380712
Rudra Prasad Bhuyan <rudraprasadbhuyan999@gmail.com>
2026-01-29 18:43:01 +0530

    Add all L05 Item Code Categories w/ select options

--- pages/3_BL_05.py
+++ pages/3_BL_05.py
@@ -1,6 +1,4 @@
 import streamlit as st
-import polars as pl
-import builtins
 from pathlib import Path
 
 from app.l05_functions import(
@@ -22,6 +20,16 @@ from app.l05_functions import(
     
     category_mapping,
     cereal_mapping,
+    pulses_mapping,
+    salt_sugar_mapping,
+    milk_mapping,
+    vegetables_mapping,
+    fruits_fresh_mapping,
+    fruits_dry_mapping,
+    nonveg_mapping,
+    edible_oil_mapping,
+    spices_mapping,
+    beverages_mapping,
 )
 
 LEVEL_05_PATH = Path("data\BL05.parquet")
@@ -36,6 +44,34 @@ st.title("Level 05")
 df = load_level_05_data(LEVEL_05_PATH)
 fdf = df.clone()
 
+CATEGORY_FN_MAP = {
+    "All Category": category_mapping,
+    "Cereal Family": cereal_mapping,
+    "Pulses Family": pulses_mapping,
+    "Salt & Sugar Family": salt_sugar_mapping,
+    "Milk Family": milk_mapping,
+    "Vegetables": vegetables_mapping,
+    "Fresh Fruits": fruits_fresh_mapping,
+    "Dry Fruits": fruits_dry_mapping,
+    "Non-Veg": nonveg_mapping,
+    "Edible Oil": edible_oil_mapping,
+    "Spices Mapping": spices_mapping,
+    "Beverages": beverages_mapping,
+}
+
+with st.expander("📦 Choose Category want to view", expanded=True):
+    
+    selected_category = st.pills(
+        label="",
+        options=list(CATEGORY_FN_MAP.keys()),
+        selection_mode="single",
+        default="All Category",
+    )
+
+category_fn = CATEGORY_FN_MAP[selected_category]   
+category_dict = category_fn()
+cat_df = category_dict_to_dataframe(category_dict, fdf)
+
 tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs([
     "t1", "t2", "t3", "t4", "t5", "t6", 
 ])
@@ -45,8 +81,7 @@ with tab1:
     st.write(df.describe())
 
 with tab2:
-    cat_df = category_dict_to_dataframe(category_mapping(), fdf)
-    
+        
     fig1 = total_consumption_qty_by_category(cat_df)
     st.plotly_chart(fig1)
     
